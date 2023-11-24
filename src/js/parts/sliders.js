6d9964d5417f9f5dc57113d0fbeb712d433e5e5d
MyCode07 <erem19992015@mail.ru>
2023-11-24 22:56:59 +0300

    add services for cities

--- src/js/parts/sliders.js
+++ src/js/parts/sliders.js
@@ -3,10 +3,10 @@ import { Autoplay, Navigation, EffectCards } from 'swiper/modules';
 
 const slideName = document.querySelector('.about-slider__name');
 const slidePos = document.querySelector('.about-slider__pos');
-const slideExpNum = document.querySelector('.about-slider .exp span');
-const slideExpText = document.querySelector('.about-slider .exp label');
-const slideParamName = document.querySelector('.about-slider .param span');
-const slideParamText = document.querySelector('.about-slider .param label');
+const slideParam1Name = document.querySelector('.about-slider .param-1 span');
+const slideParam1Text = document.querySelector('.about-slider .param-1 label');
+const slideParam2Name = document.querySelector('.about-slider .param-2 span');
+const slideParam2Text = document.querySelector('.about-slider .param-2 label');
 
 const sliders = document.querySelectorAll('.swiper');
 
@@ -51,19 +51,18 @@ export const createSliders = () => {
                         const slide = swiper.slides[swiper.activeIndex];
 
                         const name = slide.dataset.name
-                        const pos1 = slide.dataset.pos1
-                        const pos2 = slide.dataset.pos2
-                        const expNum = slide.dataset.expNum
-                        const expText = slide.dataset.expText
-                        const paramNum = slide.dataset.paramNum
-                        const paramText = slide.dataset.paramText
+                        const pos = slide.dataset.pos
+                        const param1Num = slide.dataset.param1Num
+                        const param1Text = slide.dataset.param1Text
+                        const param2Num = slide.dataset.param2Num
+                        const param2Text = slide.dataset.param2Text
 
                         slideName.textContent = name
-                        slidePos.innerHTML = pos1 + '<br>' + pos2
-                        slideExpNum.textContent = expNum
-                        slideExpText.textContent = expText
-                        slideParamName.textContent = paramNum
-                        slideParamText.textContent = paramText
+                        slidePos.innerHTML = pos
+                        slideParam1Name.textContent = param1Num
+                        slideParam1Text.textContent = param1Text
+                        slideParam2Name.textContent = param2Num
+                        slideParam2Text.textContent = param2Text
                     }
                 }
             })
