ed7e996e26c0fea4792cc8f77eb8bd0c4dafdd94
MyCode07 <erem19992015@mail.ru>
2023-12-09 10:52:11 +0300

    add new team gallery

--- src/js/parts/read-more.js
+++ src/js/parts/read-more.js
@@ -10,16 +10,36 @@ document.addEventListener('click', function (e) {
     }
 
     if (targetEl.classList.contains('view-more') && targetEl.classList.contains('view-more-img')) {
-        const hiddenItems = targetEl.closest('section').querySelectorAll('[hidden]');
+        const hiddenItems = targetEl.closest('section').querySelectorAll('.education__gallery-item[hidden]');
 
-        if (hiddenItems.length)
-            hiddenItems.forEach(item => {
-                const img = item.querySelector('img')
-                img.src = img.dataset.src
+        targetEl.classList.toggle('_active');
+        const textOutput = targetEl.querySelector('span')
+        const text = textOutput.dataset.text;
 
-                item.removeAttribute('hidden')
+        if (targetEl.classList.contains('_active')) {
+            if (hiddenItems.length)
+                hiddenItems.forEach(item => {
+                    item.classList.add('_active');
+                    const img = item.querySelector('img')
+                    const sources = item.querySelectorAll('source')
 
-                targetEl.remove();
-            })
+                    img.src = img.dataset.src
+                    if (sources.length) sources.forEach(source => source.srcset = img.dataset.src)
+
+                    item.removeAttribute('hidden')
+                })
+
+            textOutput.textContent = 'Скрыть';
+        }
+        else {
+            const hideElems = targetEl.closest('section').querySelectorAll('.education__gallery-item._active');
+            if (hideElems.length)
+                hideElems.forEach(item => {
+                    item.setAttribute('hidden', true)
+                    item.classList.remove('_active');
+                })
+
+            textOutput.textContent = text;
+        }
     }
 })
