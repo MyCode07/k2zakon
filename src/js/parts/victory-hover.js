ea2fb14d00905c9ea362d19edd176ba874c40974
MyCode07 <erem19992015@mail.ru>
2024-02-10 20:20:36 +0300

    add another changes to some pages

--- src/js/parts/victory-hover.js
+++ src/js/parts/victory-hover.js
@@ -1,20 +1,31 @@
 import { isMobile } from "../utils/isMobile.js";
 
-
 export const victoryCardsHover = () => {
     var victoryCards = document.querySelectorAll('.victory__card');
 
     if (!victoryCards) return;
 
-    victoryCards.forEach(card => {
-        const cardText = card.querySelector('.victory__card-text');
+    if (!isMobile.any()) {
+        document.addEventListener('mouseenter', ({ target: t }) => {
+            if (t.className === 'victory__card-text') {
+                t.closest('.victory__card').classList.add('_active')
+            }
+        }, true);
 
-        if (!isMobile.any()) {
-            cardText.addEventListener("mouseenter", () => card.classList.add('_active'));
-            card.addEventListener("mouseleave", () => card.classList.remove('_active'));
-        }
-        else {
-            cardText.addEventListener("click", (e) => card.classList.toggle('_active'));
-        }
-    })
+        document.addEventListener('mouseleave', e => {
+            if (e.target.className === 'victory__card-text') {
+                e.target.closest('.victory__card').classList.remove('_active')
+            }
+        }, true);
+    }
+    else {
+        document.addEventListener('click', function (e) {
+            let targetEl = e.target;
+            if (targetEl.classList.contains('victory__card-text') || targetEl.closest('.victory__card-text')) {
+                victoryCards = document.querySelectorAll('.victory__card');
+                victoryCards.forEach(card => card.classList.remove('_active'))
+                targetEl.closest('.victory__card').classList.toggle('_active')
+            }
+        })
+    }
 }
